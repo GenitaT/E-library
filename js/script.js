96c50d027a95b3d1e106fa59ad4d760038f7f889
ArberHajrizaj <ArberHajrizaj@users.noreply.github.com>
2020-10-14 16:00:57 +0200

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+//Resources animated dropdown
 $(document).ready(function(){
     $('#resources').mouseenter(function(){
         $('#resources-item').slideDown('slow');
     });
+});
+
+
+//arrow animated dropdown
+$('.arrow').on('click', function() {
+    $(this).toggleClass('active');
 });
